9dbb6fe3118ac0eb52f3ba9632af5517ade4bbae
kenbadertscher <98230845+kenbadertscher@users.noreply.github.com>
2025-07-19 09:30:37 -0400

    implement auth

--- app/root.tsx
+++ app/root.tsx
@@ -9,6 +9,8 @@ import {
 
 import type { Route } from "./+types/root";
 import "./app.css";
+import { usePuterStore } from "./lib/puter";
+import { useEffect } from "react";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -24,24 +26,30 @@ export const links: Route.LinksFunction = () => [
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
+	const { init } = usePuterStore();
 
-	  	<script src="https://js.puter.com/v2/"></script>
+	useEffect(() => {
+		init()
+	}, [init]);
 
-        {children}
-        <ScrollRestoration />
-        <Scripts />
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<head>
+			<meta charSet="utf-8" />
+			<meta name="viewport" content="width=device-width, initial-scale=1" />
+			<Meta />
+			<Links />
+			</head>
+			<body>
+
+			<script src="https://js.puter.com/v2/"></script>
+
+			{children}
+			<ScrollRestoration />
+			<Scripts />
+			</body>
+		</html>
+	);	
 }
 
 export default function App() {
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,7 +1,10 @@
 import type { Route } from "./+types/home";
-import Navbar from "../components/navbar";
+import Navbar from "~/components/Navbar";	
 import { resumes } from "../../constants";
 import ResumeCard from "~/components/ResumeCard";
+import { usePuterStore } from "~/lib/puter";
+import { useLocation, useNavigate } from "react-router";
+import { useEffect } from "react";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -11,21 +14,29 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
-	<Navbar />
-	<section className="main-section py-16">
-		<div className="page-heading">
-			<h1>Track Your Applications & Resume Ratings</h1>
-			<h2>Review your submissions and check AI-powered feedback.</h2>
-		</div>
+	const { auth } = usePuterStore();
+	const navigate = useNavigate();
 
-		{resumes.length > 0 && (
-			<div className="resumes-section">
-				{resumes.map( (resume) => (
-					<ResumeCard key={resume.id} resume={resume} />
-				))}
+	useEffect(() => {
+		if(!auth.isAuthenticated) navigate('/auth?next=/')
+	}, [auth.isAuthenticated]);
+
+  	return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+		<Navbar />
+
+		<section className="main-section py-16">
+			<div className="page-heading">
+				<h1>Track Your Applications & Resume Ratings</h1>
+				<h2>Review your submissions and check AI-powered feedback.</h2>
 			</div>
-		)}	
-	</section>
-  </main>
+
+			{resumes.length > 0 && (
+				<div className="resumes-section">
+					{resumes.map( (resume) => (
+						<ResumeCard key={resume.id} resume={resume} />
+					))}
+				</div>
+			)}	
+		</section>
+	</main>
 }
